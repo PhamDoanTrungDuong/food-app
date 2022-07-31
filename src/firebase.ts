43fec7aa827f2a9059020e13c0f1a1a2e544f885
thienanga075 <pdtduong@gmail.com>
2022-07-31 16:09:38 +0700

    deploy

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4AW3mT92nrzyXyD2LvT3IFfFQx8G0NO0",
+  authDomain: "food-app-f0c52.firebaseapp.com",
+  projectId: "food-app-f0c52",
+  storageBucket: "food-app-f0c52.appspot.com",
+  messagingSenderId: "991657763394",
+  appId: "1:991657763394:web:06073add3c33e2c43d83db"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
